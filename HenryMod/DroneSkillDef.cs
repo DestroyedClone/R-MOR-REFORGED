b716efae283c0baf14c2cb37ba06cd5424f41142
DestroyedClone <72328339+DestroyedClone@users.noreply.github.com>
2024-11-07 22:04:37 -0800

    Fixed LanguageTokens variable Updated DroneSkillDef, and excluded due to unuse

--- HenryMod/DroneSkillDef.cs
+++ HenryMod/DroneSkillDef.cs
@@ -8,33 +8,12 @@ namespace RMORMod
 {
     public class DroneSkillDef : SkillDef
 	{
-		public override SkillDef.BaseSkillInstanceData OnAssigned([NotNull] GenericSkill skillSlot)
-		{
-			return new DroneSkillDef.InstanceData
-			{
-				targetingController = skillSlot.GetComponent<RMORTargetingController>()
-			};
-		}
+        public TargetingMode targetingMode = TargetingMode.EnemiesAndAllies;
 
-		public override bool CanExecute([NotNull] GenericSkill skillSlot)
-		{
-			return DroneSkillDef.HasTarget(skillSlot) && base.CanExecute(skillSlot);
-		}
-
-		public override bool IsReady([NotNull] GenericSkill skillSlot)
-		{
-			return base.IsReady(skillSlot) && DroneSkillDef.HasTarget(skillSlot);
-		}
-
-		protected class InstanceData : SkillDef.BaseSkillInstanceData
-		{
-			public RMORTargetingController targetingController;
-		}
-
-		private static bool HasTarget([NotNull] GenericSkill skillSlot)
-		{
-			RMORTargetingController targeter = ((DroneSkillDef.InstanceData)skillSlot.skillInstanceData).targetingController;
-			return (targeter != null) ? targeter.HasTarget() : false;
-		}
-	}
+        public enum TargetingMode
+        {
+            EnemiesAndAllies,
+            EnemiesOnly
+        }
+    }
 }
